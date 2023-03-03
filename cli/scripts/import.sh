cde38eab8298352558ccee82ba7cd184bfab846f
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2023-03-03 11:34:19 +0700

    [TOOLING-1] (ii): Update file structure and import.sh script (#5)
    * Create import.sh
    
    * Update script and folder structure

--- cli/scripts/import.sh
+++ cli/scripts/import.sh
@@ -2,7 +2,8 @@
 set -o errexit
 set -o pipefail
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(pwd)
 
 usage() {
     echo $1
@@ -16,18 +17,23 @@ EOF
 
 MAJOR_VERSION=$1
 
-if [ -z "$MAJOR_VERSION" ]; then usage "ERROR | No major version provided"; exit 1; fi
+# if [ -z "$MAJOR_VERSION" ]; then usage "ERROR | No major version provided"; exit 1; fi
+if [ -z "$MAJOR_VERSION" ]; then usage "WARNING | No major version provided, fetching the latest release"; MAJOR_VERSION=v; fi
 
 echo "Import keycloak ${MAJOR_VERSION}"
 
 # find the latest git tag of major version
-TAG=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -r '.[].name' | grep "${MAJOR_VERSION:1}" | head -n 1 || true)
+TAG=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -r '.[].name' | grep "${MAJOR_VERSION:1}" | grep -v nightly | head -n 1 || true)
 
 if [ -z "$TAG" ]; then echo "ERROR | No git tag of the provided major version"; exit 1; fi
 
 echo "Found git tag $TAG in keycloak repository"
 
-echo "Create release folder of MAJOR_VERSION"
+arrIN=(${TAG//./ })
+MAJOR_VERSION="v${arrIN[0]}"
+
+
+echo "Create release folder by MAJOR_VERSION"
 # create folders
 mkdir -p "$SCRIPT_DIR/releases/$MAJOR_VERSION/latest"
 cd "$SCRIPT_DIR/releases/$MAJOR_VERSION/latest"
@@ -35,8 +41,8 @@ mkdir -p "overrides"
 touch "overrides/.gitkeep"
 mkdir -p "patches"
 touch "patches/.gitkeep"
-mkdir -p "removals"
-touch "removals/.gitkeep"
+mkdir -p "cicd"
+touch "cicd/.gitkeep"
 
 # create submodule
 if [ -d "keycloak" ]; then
