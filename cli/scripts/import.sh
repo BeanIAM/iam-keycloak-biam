3ac52c03a7fccdb1b8e271da8b8ec810d0add2f9
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2023-03-05 18:49:30 +0700

    TOOLING-1 (iv): Improve import.sh script to detect latest commit so that it can just exit (#10)

--- cli/scripts/import.sh
+++ cli/scripts/import.sh
@@ -20,12 +20,21 @@ git submodule foreach --recursive 'git fetch --tags --force'
 MAJOR_VERSION=$1
 
 # if [ -z "$MAJOR_VERSION" ]; then usage "ERROR | No major version provided"; exit 1; fi
-if [ -z "$MAJOR_VERSION" ]; then usage "WARNING | No major version provided, fetching the latest release"; MAJOR_VERSION=v; fi
+if [ -z "$MAJOR_VERSION" ]; then
+  usage "WARNING | No major version provided, fetching the latest release"; MAJOR_VERSION=v; 
+  TAG=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -rc '[.[] | select( .name | contains("nightly") | not )][0].name')
+  COMMIT_SHA=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -rc '[.[] | select( .name | contains("nightly") | not )][0].commit.sha')
+else
+  TAG=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -rc --arg majorVersion "${MAJOR_VERSION:1}" '[.[] | select( .name | contains($majorVersion)  )][0].name')
+  COMMIT_SHA=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -rc --arg majorVersion "${MAJOR_VERSION:1}" '[.[] | select( .name | contains($majorVersion)  )][0].commit.sha')
+fi
 
 echo "Import keycloak ${MAJOR_VERSION}"
 
 # find the latest git tag of major version
-TAG=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -r '.[].name' | grep "${MAJOR_VERSION:1}" | grep -v nightly | head -n 1 || true)
+# TAG=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -r '.[].name' | grep "${MAJOR_VERSION:1}" | grep -v nightly | head -n 1 || true)
+
+
 
 if [ -z "$TAG" ]; then echo "ERROR | No git tag of the provided major version"; exit 1; fi
 
@@ -34,6 +43,24 @@ echo "Found git tag $TAG in keycloak repository"
 arrIN=(${TAG//./ })
 MAJOR_VERSION="v${arrIN[0]}"
 
+git submodule status "releases/$MAJOR_VERSION/latest/keycloak"; IS_MAJOR_VERSION_EXISTING=$?
+
+if [ "$IS_MAJOR_VERSION_EXISTING" = "1" ]; then
+    echo "Major Version does not exist, importing now"
+else
+    echo "$MAJOR_VERSION already exists, check if commit is latest"
+    CURRENT_MAJOR_VERSION_COMMIT_SHA=`git submodule status releases/${MAJOR_VERSION}/latest | head -n1 | awk '{print $1;}'`
+    if [ "$CURRENT_MAJOR_VERSION_COMMIT_SHA" = "$COMMIT_SHA" ]; then
+      echo "Already latest, nothing to commit. Exiting now"
+      exit
+    else
+      echo "Commit is not latest, importing now"
+    fi
+fi
+
+# just a placeholder
+CURRENT_LATEST_MAJOR_VERSION=`ls -t releases | head -n1`
+CURRENT_LATEST_MAJOR_VERSION_COMMIT_SHA=`git submodule status releases/${CURRENT_LATEST_MAJOR_VERSION}/latest | head -n1 | awk '{print $1;}'`
 
 echo "Create release folder by MAJOR_VERSION"
 # create folders
@@ -53,6 +80,7 @@ if [ -d "keycloak" ]; then
   git submodule update .
   cd ..
 fi
+
 git submodule add -f git@github.com:keycloak/keycloak.git keycloak
 cd "keycloak"
 git checkout "$TAG"
