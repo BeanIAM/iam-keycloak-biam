be92d78939934159f471931cfeb2c93015b044b0
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2023-03-06 01:12:18 +0700

    [IAM] GitHub action workflow to import latest versions from upstream (#12)
    * IAM: Create GithubAction Workflow to import latest releases every night at 1AM
    
    * Convenient update to add remove.sh script

--- cli/scripts/import.sh
+++ cli/scripts/import.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 set -o errexit
 set -o pipefail
+set -e 
+
+if [ -z "$GITHUB_REPOSITORY" ]; then
+  echo "Please set env var for GITHUB_REPOSITORY. E.g: BeanCloudServices/iam-keycloak-bcs"
+  exit 1
+fi
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "Please set env var GITHUB_TOKEN to your GITHUB PAT (Personal Access Token)"
+  exit 1
+fi
+
+if [ -z "$GITHUB_REPOSITORY_OWNER" ]; then
+  echo "Please set env var for GITHUB_REPOSITORY_OWNER. E.g: BeanCloudServices"
+  exit 1
+fi
 
 # SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SCRIPT_DIR=$(pwd)
 
 usage() {
-    echo $1
     THIS=$(basename $0)
     cat << EOF
 Usage: 
   \$ $THIS <keycloak major version>
 
 EOF
+    echo $1
 }
 
 git submodule foreach --recursive 'git fetch --tags --force'
@@ -24,45 +40,46 @@ if [ -z "$MAJOR_VERSION" ]; then
   usage "WARNING | No major version provided, fetching the latest release"; MAJOR_VERSION=v; 
   TAG=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -rc '[.[] | select( .name | contains("nightly") | not )][0].name')
   COMMIT_SHA=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -rc '[.[] | select( .name | contains("nightly") | not )][0].commit.sha')
+  echo -e "\n - Import keycloak latest"
 else
   TAG=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -rc --arg majorVersion "${MAJOR_VERSION:1}" '[.[] | select( .name | contains($majorVersion)  )][0].name')
   COMMIT_SHA=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -rc --arg majorVersion "${MAJOR_VERSION:1}" '[.[] | select( .name | contains($majorVersion)  )][0].commit.sha')
-fi
+  echo -e "\n - Import keycloak ${MAJOR_VERSION}"
 
-echo "Import keycloak ${MAJOR_VERSION}"
+fi
 
 # find the latest git tag of major version
 # TAG=$(curl --silent "https://api.github.com/repos/keycloak/keycloak/tags" | jq -r '.[].name' | grep "${MAJOR_VERSION:1}" | grep -v nightly | head -n 1 || true)
 
 
 
-if [ -z "$TAG" ]; then echo "ERROR | No git tag of the provided major version"; exit 1; fi
+if [ -z "$TAG" ]; then echo -e "\n - ERROR | No git tag of the provided major version"; exit 1; fi
 
-echo "Found git tag $TAG in keycloak repository"
+echo -e "\n - Found git tag $TAG in keycloak repository"
 
 arrIN=(${TAG//./ })
 MAJOR_VERSION="v${arrIN[0]}"
 
-git submodule status "releases/$MAJOR_VERSION/latest/keycloak"; IS_MAJOR_VERSION_EXISTING=$?
+IS_MAJOR_VERSION_EXISTING=`git submodule status "releases/$MAJOR_VERSION/latest/keycloak"; IS_MAJOR_VERSION_EXISTING=$?; echo $IS_MAJOR_VERSION_EXISTING`
 
 if [ "$IS_MAJOR_VERSION_EXISTING" = "1" ]; then
-    echo "Major Version does not exist, importing now"
+    echo -e "\n - Major Version does not exist, importing now"
 else
-    echo "$MAJOR_VERSION already exists, check if commit is latest"
+    echo -e "\n - $MAJOR_VERSION already exists, check if commit is latest"
     CURRENT_MAJOR_VERSION_COMMIT_SHA=`git submodule status releases/${MAJOR_VERSION}/latest | head -n1 | awk '{print $1;}'`
     if [ "$CURRENT_MAJOR_VERSION_COMMIT_SHA" = "$COMMIT_SHA" ]; then
-      echo "Already latest, nothing to commit. Exiting now"
+      echo -e "\n - Already latest, nothing to commit. Exiting now"
       exit
     else
-      echo "Commit is not latest, importing now"
+      echo -e "\n - Commit is not latest, importing now"
     fi
 fi
 
 # just a placeholder
-CURRENT_LATEST_MAJOR_VERSION=`ls -t releases | head -n1`
-CURRENT_LATEST_MAJOR_VERSION_COMMIT_SHA=`git submodule status releases/${CURRENT_LATEST_MAJOR_VERSION}/latest | head -n1 | awk '{print $1;}'`
+# CURRENT_LATEST_MAJOR_VERSION=`ls -t releases | head -n1`
+# CURRENT_LATEST_MAJOR_VERSION_COMMIT_SHA=`git submodule status releases/${CURRENT_LATEST_MAJOR_VERSION}/latest | head -n1 | awk '{print $1;}'`
 
-echo "Create release folder by MAJOR_VERSION"
+echo -e "\n - Create release folder by MAJOR_VERSION"
 # create folders
 mkdir -p "$SCRIPT_DIR/releases/$MAJOR_VERSION/latest"
 cd "$SCRIPT_DIR/releases/$MAJOR_VERSION/latest"
@@ -84,3 +101,21 @@ fi
 git submodule add -f git@github.com:keycloak/keycloak.git keycloak
 cd "keycloak"
 git checkout "$TAG"
+
+cd $SCRIPT_DIR
+
+NEW_BRANCH="releases/${MAJOR_VERSION}/${TAG}"
+echo -e "\n - Create a new branch $NEW_BRANCH"
+git checkout -b $NEW_BRANCH
+echo -e "\n - Pushing to the new branch"
+git commit -m "IMPORT: v${TAG}"
+git push --set-upstream origin $NEW_BRANCH
+
+echo -e "\n - Create a new PR"
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$GITHUB_REPOSITORY/pulls \
+  -d "{\"title\":\"IMPORT: v${TAG} \",\"body\":\" \",\"head\":\"$GITHUB_REPOSITORY_OWNER:$NEW_BRANCH\",\"base\":\"master\"}"
