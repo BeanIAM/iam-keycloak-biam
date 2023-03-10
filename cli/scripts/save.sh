cbd64e9d7eabfa260336e27909b0dfe4c45a8506
nhuthm080280 <nhuthm080280@gmail.com>
2023-03-10 13:37:25 +0700

    update save.sh

--- cli/scripts/save.sh
+++ cli/scripts/save.sh
@@ -6,12 +6,12 @@ if [ -z "$MAJOR_VERSION" ]; then
     echo "Please specify the version which you want to save"
     exit 1
 fi
-SUB_MODULE_KEYLOAK_PATH="../../releases/${MAJOR_VERSION}/latest/keyload"
+SUB_MODULE_KEYLOAK_PATH="releases/${MAJOR_VERSION}/latest/keycloak"
 TARGET_BRANCH="origin/master"
 
 #.github folder should be copied to cicd folder
 echo "copy .github folder to cicd"
-cp -r ../../.github/ ../../releases/${MAJOR_VERSION}/cicd/
+cp -r ./.github/ releases/${MAJOR_VERSION}/cicd/
 
 echo "Check git diff"
 #reference
@@ -26,10 +26,10 @@ echo "Check git diff"
   #B have had their pairing Broken
   #* All-or-none
 #Modifications to managed files as well as File renaming should be saved in patches folder as a modifications.diff file.
-git diff --pretty --diff-filter=RM ${TARGET_BRANCH} ${SUB_MODULE_KEYLOAK_PATH} > ../../releases/${MAJOR_VERSION}/latest/patches/modifications.diff
+git diff --pretty --diff-filter=RM ${TARGET_BRANCH} ${SUB_MODULE_KEYLOAK_PATH} > releases/${MAJOR_VERSION}/latest/patches/modifications.diff
 
 #File deletions should be saved in patches folder as a removals.diff file.
-git diff --pretty --diff-filter=D ${TARGET_BRANCH} ${SUB_MODULE_KEYLOAK_PATH} > ../../releases/${MAJOR_VERSION}/latest/patches/removals.diff
+git diff --pretty --diff-filter=D ${TARGET_BRANCH} ${SUB_MODULE_KEYLOAK_PATH} > releases/${MAJOR_VERSION}/latest/patches/removals.diff
 
 #Any new files that were added should be copied to overrides folder.
 for modification in $(git diff --name-only --pretty --diff-filter=A ${TARGET_BRANCH} ${SUB_MODULE_KEYLOAK_PATH});
