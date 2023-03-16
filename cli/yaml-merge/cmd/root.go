a0d11913164f5000327a68a7fd1e37b8a70132d5
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2023-03-16 11:09:00 +0700

    Update cli/yaml-merge/cmd/root.go

--- cli/yaml-merge/cmd/root.go
+++ cli/yaml-merge/cmd/root.go
@@ -16,7 +16,7 @@ const (
 	LatestDir   = "latest"
 	PatchesDir  = "patches"
 	KeycloakDir = "keycloak"
-	DevDir      = "dev"
+	DevDir      = "build"
 )
 
 // rootCmd represents the base command when called without any subcommands
