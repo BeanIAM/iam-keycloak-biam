9c849405be0b1d50c2b7b08566038a12134c674d
Neo Huynh <nhuthm080280@gmail.com>
2023-04-14 10:16:20 +0700

    fix save.sh and add add kafka-spi to overrides folder (#37)
    * fix save.sh and add add kafka-spi to overrides folder
    
    * fix save.sh and add add kafka-spi to overrides folder

--- cli/scripts/save.sh
+++ cli/scripts/save.sh
@@ -7,6 +7,7 @@ if [ -z "$MAJOR_VERSION" ]; then
     exit 1
 fi
 SUB_MODULE_KEYCLOAK_PATH="releases/${MAJOR_VERSION}/latest/keycloak"
+echo "Submodule path: " ${SUB_MODULE_KEYCLOAK_PATH}
 TARGET_BRANCH="origin/master"
 
 #.github folder should be copied to cicd folder
@@ -14,6 +15,8 @@ echo "copy upstream .github folder to cicd"
 LATEST_RELEASE_PATH=releases/${MAJOR_VERSION}/latest
 cp -r ${LATEST_RELEASE_PATH}/keycloak/.github ${LATEST_RELEASE_PATH}/cicd/
 
+# navigate to submodule
+cd ${SUB_MODULE_KEYCLOAK_PATH}
 echo "Check git diff"
 #reference
   #A Added
@@ -27,14 +30,18 @@ echo "Check git diff"
   #B have had their pairing Broken
   #* All-or-none
 #Modifications to managed files as well as File renaming should be saved in patches folder as a modifications.diff file.
-git diff --pretty --diff-filter=RM ${TARGET_BRANCH} ${SUB_MODULE_KEYCLOAK_PATH} > releases/${MAJOR_VERSION}/latest/patches/modifications.diff
+git diff --submodule=diff --pretty --diff-filter=RM HEAD > ../patches/modifications.diff
 
 #File deletions should be saved in patches folder as a removals.diff file.
-git diff --pretty --diff-filter=D ${TARGET_BRANCH} ${SUB_MODULE_KEYCLOAK_PATH} > releases/${MAJOR_VERSION}/latest/patches/removals.diff
+git diff --submodule=diff --pretty --diff-filter=D HEAD > ../patches/removals.diff
 
 #Any new files that were added should be copied to overrides folder.
-for modification in $(git diff --name-only --pretty --diff-filter=A ${TARGET_BRANCH} ${SUB_MODULE_KEYCLOAK_PATH});
-do cp $modification releases/${MAJOR_VERSION}/latest/overrides; done
+git add .
+for modification in $(git diff --submodule=diff --name-only --pretty --diff-filter=A HEAD);
+do
+  rsync -avz --relative "$modification" ../overrides;
+done
+for file in $(git diff master --submodule=submodule-name | grep '^diff --git' | cut -d' ' -f3); do cp "$file" dest-folder; done
 
 #When every step above has been done, git keycloak submodule should be git reset --hard to discard all changes.
-git restore --source=HEAD --staged --worktree -- ${SUB_MODULE_KEYCLOAK_PATH}
+git reset --hard HEAD
